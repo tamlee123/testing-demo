82ac30ca99982a071f07b8011b1a5f215b4f37a6
tam <cherykim123@gmail.com>
2020-12-17 15:47:18 -0800

    better approach to create mock funtion by jest.fn()

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -88,16 +88,14 @@ describe('applyDiscount', () => {
 
 describe('notifyCustomer', () => {
     it('should send an email to the customer', () => {
-        db.getCustomerSync = function(customerId) {
-            return {email: 'a'};
-        }
-        
-        let mailSent = false;
-        mail.send = function(email, message){
-            mailSent = true;
-        }
+        db.getCustomerSync = jest.fn().mockReturnValue({email: 'a'});
+        mail.send = jest.fn();
 
         lib.notifyCustomer({customerID: 1});
-        expect(mailSent).toBe(true);
+
+        expect(mail.send).toHaveBeenCalled();
+        expect(mail.send.mock.calls[0] [0]).toBe('a');
+        expect(mail.send.mock.calls[0] [1]).toMatch(/order/);
+
     });
 });

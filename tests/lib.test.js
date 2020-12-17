28a41956edc93cbdbab751ac97c3cc943a14fb12
tam <cherykim123@gmail.com>
2020-12-16 17:07:29 -0800

    Testing string

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -17,3 +17,10 @@ describe('absolute', () => {
     });
 });
 
+describe('greet', ()=> {
+    it('should return the greeting message', () => {
+        const result = lib.greet('Mosh');
+        expect(result).toMatch(/Mosh/);
+        expect(result).toContain('Mosh');
+    });
+});
